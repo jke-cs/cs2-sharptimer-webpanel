9ff959e6cb27c1c03bb6c18c45ded7e549e5415c
JKE <129677512+jke-cs@users.noreply.github.com>
2024-09-26 22:52:55 +0200

    Update page.tsx

--- app/maprecords/page.tsx
+++ app/maprecords/page.tsx
@@ -56,7 +56,6 @@ export default function MapRecordsPage() {
                   onClick={() => toggleMap(mapName)}
                   className="relative w-full flex items-center justify-between p-4 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 rounded-lg shadow transition-all duration-200 ease-in-out overflow-hidden"
                 >
-                  {/* Image Container */}
                   <div
                     className="relative h-16 w-1/4 flex-shrink-0 mr-4 bg-cover bg-center rounded-lg"
                     style={{
@@ -65,28 +64,25 @@ export default function MapRecordsPage() {
                       backgroundPosition: 'center',
                     }}
                   >
-                    {/* Gradient Fade on the Left */}
+
                     <div className="absolute inset-0 left-0 bg-gradient-to-r from-gray-200 via-transparent to-transparent dark:from-gray-700 z-10"></div>
                   </div>
 
-                  {/* Map Name */}
                   <span className="relative z-20 font-semibold text-lg text-gray-800 dark:text-white flex-1 text-left">
                     {mapName}
                   </span>
                   
-                  {/* Arrow Icon */}
+
                   <span className="relative z-20 text-gray-800 dark:text-white">
                     {expandedMaps.includes(mapName) ? <FaChevronDown /> : <FaChevronRight />}
                   </span>
                 </button>
-                {/* Map Records (Expand/Collapse) */}
                 {expandedMaps.includes(mapName) && (
                   <div className="mt-2 space-y-2 pl-4 pr-4 overflow-hidden transition-all duration-300 ease-in-out">
                     {records
                       .sort((a, b) => a.FormattedTime.localeCompare(b.FormattedTime)) 
                       .map((record, index) => (
                         <div key={index} className="p-2 bg-gray-100 dark:bg-gray-800 rounded-lg flex justify-between shadow">
-                          {/* Rank Number */}
                           <span className="mr-2 font-semibold text-gray-700 dark:text-gray-300">
                             {index + 1} 
                           </span>
