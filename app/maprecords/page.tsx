fb0bb3bd42b10a5dce579a17f513bfc3dee8dcfb
Krigsgaldr793 <koestler.johannes1997@gmail.com>
2024-09-26 18:14:24 +0200

    update

--- app/maprecords/page.tsx
+++ app/maprecords/page.tsx
@@ -1,13 +1,15 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import LoadingSpinner from '@/components/LoadingSpinner'
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-300">
       <Header />
       <main className="flex-grow container mx-auto px-4 py-12">
-        <p>SOON</p>
+        <LoadingSpinner/>
       </main>
+
       <Footer />
     </div>
   )
